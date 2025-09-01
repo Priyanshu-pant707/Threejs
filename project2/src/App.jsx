785c54ecf846af59cf2e265129410be03e871624
Escape <Escape>
2025-09-01 15:26:53 +0530

    update three drie

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
  
   return <>
     <div class="overlay"></div>
-  <Canvas  className='z-20' camera={{fov:35}}>
+  <Canvas  className='canva' camera={{fov:35}}>
     <OrbitControls />
     <ambientLight />
     <Scene/>
@@ -19,6 +19,11 @@ const App = () => {
   <h1 className='text-9xl font-bold '>Priyanshu Pant</h1>
   <p className='text-2xl text-yellow-300 mt-5 font'>3D Cylinder with Texture</p>
   
+ </div>
+
+
+ <div className="three absolute top-[10%] left-[10%] ">
+  <h1 className='text-[160px] font-black text-white  '>REACT THREE DRIE</h1>
  </div>
   
   </>
